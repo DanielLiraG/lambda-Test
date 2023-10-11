efec0bddd8b8f557ba9d74fcc60886ad2c5fa370
orlandobricenob <orlando.briceno.blanco@gmail.com>
2023-10-11 07:18:25 -0400

    build: Removed build command and added esbuild for compile TS

--- lambdasTemplate/execute.sh
+++ lambdasTemplate/execute.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 npm run lint:fix
-npm run build
 sam build
-sudo sam local invoke lambdasTemplate -e events/events.json 2>&1 | tr "\r" "\n"
+sudo sam local invoke PokeData -e events/events.json 2>&1 | tr "\r" "\n"
